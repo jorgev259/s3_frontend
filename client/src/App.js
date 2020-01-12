b168167273431f560a8c3c4e8f3d32536d81d89f
jorgev259 <jorgev259@outlook.es>
2020-01-11 22:48:02 -0600

    fix something

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ping from 'web-ping'
 import './css/App.css'
 
 import FileList from './js/FileList'
@@ -17,21 +16,8 @@ export default class App extends React.Component {
   componentDidMount () {
     const context = this
     get('api/regions').then(res => {
-      const promises = Object.keys(res.data).map(region => {
-        return new Promise((resolve, reject) => {
-          ping(res.data[region].endpoint).then(function (delta) {
-            resolve({ region: region, ms: delta, public: res.data[region].public })
-          }).catch(function (err) {
-            console.error('Could not ping remote URL', err)
-            resolve({ region: region, ms: 9999999 })
-          })
-        })
-      })
-
-      Promise.all(promises).then(result => {
-        const sorted = result.sort((a, b) => (a.ms > b.ms) ? 1 : -1)
-        context.setState({ regions: sorted, region: sorted[0].region })
-      })
+      const regions = Object.keys(res.data)
+      context.setState({ regions: regions, region: regions[0].region })
     })
   }
 
